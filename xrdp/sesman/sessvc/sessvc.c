8d657053c7e582ad885e642fc1a8146f0780eaa3
shikaidong <shikaidong@nfschina.com>
2015-10-22 18:17:14 +0800

    rollback debug deal

--- xrdp/sesman/sessvc/sessvc.c
+++ xrdp/sesman/sessvc/sessvc.c
@@ -35,24 +35,17 @@ Author David LECHEVALIER <david@ulteo.com> 2012
 #include "thread_calls.h"
 #include "arch.h"
 #include "parse.h"
-#include<stdarg.h>
-#include<string.h>
-#include<errno.h>
 
 static int g_term = 0;
 static int wm_pid;
 static int x_pid;
 static int chansrv_pid;
 
-int debug_log(char* file, int line, char* format, ...);
-#define FINFO __FILE__,__LINE__
-
-
 /*****************************************************************************/
 	void DEFAULT_CC
 term_signal_handler(int sig)
 {
-	debug_log(FINFO,"xrdp-sessvc: term_signal_handler: got signal %d", sig);
+	g_writeln("xrdp-sessvc: term_signal_handler: got signal %d", sig);
 	g_term = 1;
 	g_sigterm(wm_pid);
 	g_waitpid(wm_pid);
@@ -62,12 +55,6 @@ term_signal_handler(int sig)
 
 	g_sigterm(chansrv_pid);
 	g_waitpid(chansrv_pid);
-}
-
-	void DEFAULT_CC
-debug_term_signal_handler(int sig)
-{
-	debug_log(FINFO,"xrdp-sessvc: term_signal_handler: got signal %d", sig);
 }
 
 /*****************************************************************************/
@@ -84,7 +71,7 @@ send_disconnect(char* username)
 	admin_socket = g_unix_connect("/var/spool/xrdp/xrdp_management");
 	if (admin_socket < 0)
 	{
-		debug_log(FINFO,"xrdp-sessvc: Unable to connect to session manager, %s", strerror(g_get_errno()));
+		g_writeln("xrdp-sessvc: Unable to connect to session manager, %s", strerror(g_get_errno()));
 		rv = 1;
 		return rv;
 	}
@@ -99,7 +86,7 @@ send_disconnect(char* username)
 	res = g_tcp_send(admin_socket, s->data, size, 0);
 	if (res != size)
 	{
-		debug_log(FINFO,"Error while sending data %s",strerror(g_get_errno()));
+		g_writeln("Error while sending data %s",strerror(g_get_errno()));
 		rv = 1;
 	}
 	free_stream(s);
@@ -112,7 +99,7 @@ send_disconnect(char* username)
 	void DEFAULT_CC
 nil_signal_handler(int sig)
 {
-	debug_log(FINFO,"xrdp-sessvc: nil_signal_handler: got signal %d", sig);
+	g_writeln("xrdp-sessvc: nil_signal_handler: got signal %d", sig);
 }
 
 /******************************************************************************/
@@ -146,13 +133,9 @@ main(int argc, char** argv)
 	char exe_path[262];
 	char *username;
 
-	//g_system("ulimit -c unlimited");
-	
-	
-	debug_log(FINFO,"xrdp-sessvc: the pwd %s", g_getenv("PWD"));
 	if (argc < 4)
 	{
-		debug_log(FINFO,"xrdp-sessvc: exiting, not enough params");
+		g_writeln("xrdp-sessvc: exiting, not enough params");
 		return 1;
 	}
 
@@ -160,28 +143,26 @@ main(int argc, char** argv)
 	wm_pid = g_atoi(argv[2]);
 	username = argv[3];
 
-	debug_log(FINFO,"xrdp-sessvc: waiting for X (pid %d) and WM (pid %d)",
+	g_writeln("xrdp-sessvc: waiting for X (pid %d) and WM (pid %d)",
 			x_pid, wm_pid);
 	/* run xrdp-chansrv as a seperate process */
 	chansrv_pid = g_fork();
 	if (chansrv_pid == -1)
 	{
-		debug_log(FINFO,"xrdp-sessvc: fork error");
+		g_writeln("xrdp-sessvc: fork error");
 		return 1;
 	}
 	else if (chansrv_pid == 0) /* child */
 	{
-		debug_log(FINFO,"xrdp-sessvc: g_execvp xrdp-chansrv");
 		g_set_current_dir(XRDP_SBIN_PATH);
 		g_snprintf(exe_path, 261, "%s/xrdp-chansrv", XRDP_SBIN_PATH);
 		g_execlp3(exe_path, "xrdp-chansrv", username);
 		/* should not get here */
-		debug_log(FINFO,"xrdp-sessvc: g_execvp failed: %s ", strerror(g_get_errno()));
+		g_writeln("xrdp-sessvc: g_execvp failed: %s ", strerror(g_get_errno()));
 		g_exit(1);
 	}
 	lerror = 0;
 
-	debug_log(FINFO,"the kill child status %d",kill(chansrv_pid,0));
 	g_signal_kill(term_signal_handler); /* SIGKILL */
 	g_signal_terminate(term_signal_handler); /* SIGTERM */
 	g_signal_user_interrupt(term_signal_handler); /* SIGINT */
@@ -197,47 +178,29 @@ main(int argc, char** argv)
 	{
 		lerror = g_get_errno();
 	}
-	debug_log(FINFO,"xrdp-sessvc: WM %d is dead (waitpid said %d, errno is %d) "
-			"exiting... %d %s", wm_pid,ret, lerror, errno, strerror(errno));
+	g_writeln("xrdp-sessvc: WM is dead (waitpid said %d, errno is %d) "
+			"exiting...", ret, lerror);
 	/* kill channel server */
-	debug_log(FINFO,"xrdp-sessvc: stopping channel server");
+	g_writeln("xrdp-sessvc: stopping channel server");
 	g_sigterm(chansrv_pid);
 	ret = g_waitpid(chansrv_pid);
 	while ((ret == 0) && !g_term)
 	{
 		ret = g_waitpid(chansrv_pid);
 	}
-	//chansrv_cleanup(chansrv_pid);
+	chansrv_cleanup(chansrv_pid);
 	/* kill X server */
-	debug_log(FINFO,"xrdp-sessvc: stopping X server");
+	g_writeln("xrdp-sessvc: stopping X server");
 	g_sigterm(x_pid);
 	ret = g_waitpid(x_pid);
 	while ((ret == 0) && !g_term)
 	{
 		ret = g_waitpid(x_pid);
 	}
-	debug_log(FINFO,"xrdp-sessvc: clean exit");
+	g_writeln("xrdp-sessvc: clean exit");
 	if (send_disconnect(username) != 0)
 	{
-		debug_log(FINFO,"xrdp-sessvc: Unable to send disconnect action");
+		g_writeln("xrdp-sessvc: Unable to send disconnect action");
 	}
-	return 0;
-}
-
-int debug_log(char* file, int line, char* format, ...)
-{
-	FILE *fp = NULL;
-	va_list ap;
-	fp = fopen("/debug/sessvc_debug.log","a");
-	if(fp == NULL)
-	{
-		return -1;
-	}
-	fprintf(fp,"%s %d %d:",file,line,g_getpid());
-	va_start(ap,format);
-	vfprintf(fp,format,ap);
-	va_end(ap);
-	fprintf(fp,"\n");
-	fclose(fp);
 	return 0;
 }
